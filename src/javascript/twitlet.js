770dd066ed9213730c1def16015f6d3d710f70a0
KITAZAKI Shigeru <skitazaki@gmail.com>
2011-06-11 22:49:37 +0900

    Renamed javascript samples and their relared files.

--- src/javascript/twitlet.js
+++ src/javascript/twitlet.js
@@ -4,6 +4,6 @@
  *  <http://groundwalker.com/blog/2007/02/javascript_escape_encodeuri_encodeuricomponent_.html>`_
  */
 javascript:(function(w, t, h){
-    w.open("http://twitter.com/?status=" + encodeURIComponent("\"" + t + "\" " + h));
+    w.open("http://twitter.com/?status=" + encodeURIComponent(" / " + t + " - " + h));
 }(window, document.title, location.href))
 
