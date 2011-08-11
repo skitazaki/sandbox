77ef9b8d9b4b3d211a45b2ea3f72ddab5d35dc03
KITAZAKI Shigeru <skitazaki@gmail.com>
2011-08-11 22:03:45 +0900

    Improved blog convertor, and write a article.

--- src/python/rst2html-custom-tpl.py
+++ src/python/rst2html-custom-tpl.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__doc__ = """\
+__doc__ = """
 python %prog {manuscript}
 
-Convert reST into HTML text with default template and custom one.
+Parse a reST manuscript and write out it in two way HTML formats.
+A file whose suffix is ".html" is for preview, and a file whose suffix
+is ".txt" is for blogger's article.
 """
 
-import logging
 import optparse
-import os.path
-import sys
+import os
+import tempfile
 
 from docutils.core import publish_cmdline
 
-HTML_PREVIEW = 'preview.html'
-HTML_SOURCE = 'preview.txt'
-RST_TEMPLATE_SOURCE = 'rst2html-custom-tpl.tpl'
-
+# see: lib/python2.7/site-packages/docutils/writers/html4css1/template.txt
+RST_TEMPLATE_SOURCE = '''
+%(body)s
+'''
 
 def parse_args():
     parser = optparse.OptionParser(__doc__)
@@ -31,18 +32,28 @@ def parse_args():
 
 
 def publish_restructured_text(manuscript):
-    argv = ["--template=%s" % (RST_TEMPLATE_SOURCE,), manuscript, HTML_SOURCE]
-    publish_cmdline(writer_name='html', argv=argv)
-    argv = [manuscript, HTML_PREVIEW]
-    publish_cmdline(writer_name='html', argv=argv)
+    fname = tempfile.mktemp()
+    with open(fname, 'w') as temp:
+        temp.write(RST_TEMPLATE_SOURCE)
+    basic_option = ['--strip-comments']
+    basename, _ = os.path.splitext(os.path.basename(manuscript))
+    source = '%s.txt' % (basename,)
+    preview = '%s.html' % (basename,)
+    argv = ['--template=%s' % (fname,), manuscript, source]
+    publish_cmdline(writer_name='html', argv=basic_option+argv)
+    argv = [manuscript, preview]
+    publish_cmdline(writer_name='html', argv=basic_option+argv)
+    os.unlink(fname)
+    print "Write as single HTML page and only body element."
+    print "  single page : %s (%dbytes)" % (preview, os.path.getsize(preview))
+    print "  only body   : %s (%dbytes)" % (source, os.path.getsize(source))
 
 
 def main():
     manuscript = parse_args()
 
     if not os.path.exists(manuscript):
-        logging.fatal("%s is not found." % (manuscript,))
-        sys.exit(1)
+        raise SystemExit("%s is not found." % (manuscript,))
 
     publish_restructured_text(manuscript)
 
@@ -51,8 +62,8 @@ def test():
     manuscript = "../../ChangeLog"
     publish_restructured_text(manuscript)
 
-    assert os.path.exists(HTML_PREVIEW)
-    assert os.path.exists(HTML_SOURCE)
+    assert os.path.exists('ChangeLog.txt')
+    assert os.path.exists('ChangeLog.html')
 
 if __name__ == '__main__':
     main()
