78122876f94040f8def80db7de6bb8c5e5e0ec89
KITAZAKI Shigeru <skitazaki@gmail.com>
2013-01-22 22:41:06 +0900

    improve sort order

--- src/bash/svncounts-one-month-commits.sh
+++ src/bash/svncounts-one-month-commits.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 # shows commit counts per committer.
 
-days=30
-since=`cat <<EOT |python
+if [ -z "$since" ]
+then
+    days=30
+    since=`cat <<EOT |python
 import datetime as d; print(d.date.today() - d.timedelta(days=$days))
 EOT`
-
+fi
 echo "since: $since"
 for d in ${*-.}
 do
     [ -d $d/.svn ] || continue
     echo "directory: $d"
-    svn log -r {$since}:HEAD $d |
-    grep '^r[0-9]' | cut -d"|" -f2 | sort | uniq -c
+    svn log -r {$since}:HEAD --stop-on-copy -q $d |
+        grep '^r[0-9]' | cut -d"|" -f2 | sort | uniq -c | sort -nr
 done
 
