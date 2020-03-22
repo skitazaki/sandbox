49e52d72bd90abee4c635ba72d47a08f5a6f066e
Shigeru Kitazaki <skitazaki@gmail.com>
2020-03-22 18:44:37 +0900

    Upgrade to argparse

--- src/python/sandboxlib/__init__.py
+++ src/python/sandboxlib/__init__.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 
-"""Misc. utilities.
-Use these after copying to your projects.
+"""Misc. utilities on a sandbox project.
 """
 
-__title__ = 'sandboxlib'
-__version__ = '0.0.1'
-__author__ = 'Shigeru Kitazaki'
+__title__ = "sandboxlib"
+__version__ = "0.0.1"
+__author__ = "Shigeru Kitazaki"
 
+import argparse
 import datetime
 import logging
 import os
-import optparse  # TODO: Migrate to argparse
 
-DEFAULT_ENCODING = 'utf-8'
-logger = logging.getLogger('sandbox')
+DEFAULT_ENCODING = "utf-8"
+logger = logging.getLogger("sandbox")
 
 
 def _init_logger():
     s = logging.StreamHandler()
-    s.setFormatter(logging.Formatter('%(asctime)-15s %(levelname)-8s %(message)s'))
+    s.setFormatter(logging.Formatter("%(asctime)-15s %(levelname)-8s %(message)s"))
     logger.setLevel(logging.DEBUG)
     logger.addHandler(s)
+
 
 _init_logger()
 
@@ -30,8 +30,7 @@ class ArgumentError(Exception):
     pass
 
 
-def parse_args(doc=None, minargc=0, maxargc=None,
-               prefook=None, postfook=None):
+def parse_args(doc=None, nargs="*", prefook=None, postfook=None) -> argparse.Namespace:
     """Generic command line argument parser.
     * Handle configuration file and base directory.
     * Handle input and output encoding.
@@ -44,57 +43,88 @@ def parse_args(doc=None, minargc=0, maxargc=None,
     :param postfook function:
     :rtype: normal arguments except options.
     """
-    parser = optparse.OptionParser(doc)
-    parser.add_option("-f", "--file", dest="filename",
-        help="setting file", metavar="FILE")
-    parser.add_option("-o", "--out", dest="output",
-        help="output file", metavar="FILE")
-    parser.add_option("--basedir", dest="basedir",
-        help="base directory", default=os.getcwd())
-    parser.add_option("--input-encoding", dest="enc_in",
-        help="encoding of input source", default=DEFAULT_ENCODING)
-    parser.add_option("--output-encoding", dest="enc_out",
-        help="encoding of output destination", default=DEFAULT_ENCODING)
-    parser.add_option("-v", "--verbose", dest="verbose",
-        default=False, action="store_true", help="verbose mode")
-    parser.add_option("-q", "--quiet", dest="quiet",
-        default=False, action="store_true", help="quiet mode")
+    parser = argparse.ArgumentParser(description=doc)
+    parser.add_argument(
+        "-f", "--file", dest="filename", help="setting file", metavar="FILE"
+    )
+    parser.add_argument(
+        "-o", "--out", dest="output", help="output file", metavar="FILE"
+    )
+    parser.add_argument(
+        "--basedir", dest="basedir", help="base directory", default=os.getcwd()
+    )
+    parser.add_argument(
+        "--input-encoding",
+        dest="enc_in",
+        help="encoding of input source",
+        default=DEFAULT_ENCODING,
+    )
+    parser.add_argument(
+        "--output-encoding",
+        dest="enc_out",
+        help="encoding of output destination",
+        default=DEFAULT_ENCODING,
+    )
+
+    parser.add_argument("files", nargs=nargs, help="input files", metavar="FILE")
+
+    loglevel = parser.add_mutually_exclusive_group()
+
+    loglevel.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="count",
+        default=0,
+        help="increase logging verbosity",
+    )
+
+    loglevel.add_argument(
+        "-q",
+        "--quiet",
+        dest="quiet",
+        default=False,
+        action="store_true",
+        help="set logging to quiet mode",
+    )
 
     if prefook:
         prefook(parser)
 
-    opts, args = parser.parse_args()
-
-    if len(args) < minargc:
-        parser.error("Lacking argument(s).")
-    if maxargc is not None and len(args) > maxargc:
-        parser.error("Too many arguments.")
+    args = parser.parse_args()
 
     if postfook:
         try:
-            postfook(opts, args)
+            postfook(args)
         except ArgumentError as e:
             parser.error(e)
 
-    if opts.filename and not os.path.exists(opts.filename):
+    if args.filename and not os.path.exists(args.filename):
         parser.error("Configuration file was not found.")
-    if opts.output and os.path.exists(opts.output):
-        logger.warn("\"%s\" already exists.", opts.output)
+    if args.output and os.path.exists(args.output):
+        logger.warn('"%s" already exists.', args.output)
 
-    if opts.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    elif not opts.quiet:
-        logging.basicConfig(level=logging.INFO)
+    if args.quiet:
+        logger.setLevel(logging.CRITICAL)
+    elif args.verbose >= 3:
+        logger.setLevel(logging.DEBUG)
+    elif args.verbose >= 2:
+        logger.setLevel(logging.ERROR)
+    elif args.verbose >= 1:
+        logger.setLevel(logging.WARN)
+    else:
+        logger.setLevel(logging.INFO)
 
-    return opts, args
+    return args
 
 
-def check_file_path(opts, files):
+def check_file_path(args):
+    files = args.files
     if not files:
         raise ArgumentError("No input file.")
     notfound = filter(lambda f: not os.path.exists(f), files)
     if notfound:
-        raise ArgumentError("File not found: " + ','.join(notfound))
+        raise ArgumentError("File not found: " + ",".join(notfound))
 
 
 ZERO = datetime.timedelta(0)
@@ -112,9 +142,10 @@ class UTC(datetime.tzinfo):
     def dst(self, dt):
         return ZERO
 
+
 NOW = datetime.datetime.now(UTC())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     boilerplate = '''#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -123,7 +154,6 @@ if __name__ == '__main__':
 Description is here.
 """
 
-import codecs
 import logging
 import sys
 
@@ -138,8 +168,8 @@ class Processor(object):
     def process_file(self, fname, encoding):
         logging.info("Start processing: %s", fname)
         try:
-            self.process(codecs.open(fname, encoding=encoding))
-        except Exception, e:
+            self.process(open(fname, encoding=encoding))
+        except Exception as e:
             logging.error(e)
         logging.info("End processing: %s", fname)
 
@@ -148,13 +178,14 @@ class Processor(object):
 
 
 def main():
-    opts, files = parse_args(doc=__doc__, postfook=check_file_path)
+    args = parse_args(doc=__doc__, postfook=check_file_path)
+    files = args.files
     writer = None
-    if opts.output:
-        writer = codecs.open(opts.output, 'w', opts.enc_out)
+    if args.output:
+        writer = open(args.output, 'w', args.enc_out)
     p = Processor(writer)
     for fname in files:
-        p.process_file(fname, opts.enc_in)
+        p.process_file(fname, args.enc_in)
     if writer:
         writer.close()
 
@@ -168,7 +199,7 @@ def test():
     io.seek(0)
     assert False
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 '''
     print(boilerplate)
