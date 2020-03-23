4c533f647ef58d2fa3edc97acaeeace1da954724
Shigeru Kitazaki <skitazaki@gmail.com>
2020-03-23 21:37:25 +0900

    Migrate to Python3

--- src/python/playground/date-expand.py
+++ src/python/playground/date-expand.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" Expand date string
+"""Expand date string
 
 Examples::
 
@@ -45,7 +45,7 @@ Examples::
     20121031
     20121030
 
-    $ python date-expand.py -v
+    $ python date-expand.py -vvv
     DEBUG:root:Start: 2012-11-05
     20121105
 """
@@ -55,46 +55,35 @@ import datetime
 import logging
 import sys
 
+from sandboxlib import parse_args
+
 TODAY = datetime.date.today()
 
 
+def setup_arguments(parser):
+    parser.add_argument("--year", type=int, default=TODAY.year)
+    parser.add_argument("--month", type=int, default=TODAY.month)
+    parser.add_argument("--day", type=int, default=TODAY.day)
+    parser.add_argument("--format", default="%Y%m%d")
+    parser.add_argument("--days", type=int, default=1)
+    parser.add_argument("--reverse", default=False, action="store_true")
+    parser.add_argument(
+        "-o", "--output", type=argparse.FileType("w"), default=sys.stdout
+    )
+
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--year', type=int, default=TODAY.year)
-    parser.add_argument('--month', type=int, default=TODAY.month)
-    parser.add_argument('--day', type=int, default=TODAY.day)
-    parser.add_argument('--format', default='%Y%m%d')
-    parser.add_argument('--days', type=int, default=1)
-    parser.add_argument('--reverse', default=False, action="store_true")
-
-    parser.add_argument('--output',
-        type=argparse.FileType('w'), default=sys.stdout)
-
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--verbose", dest="verbose",
-                default=False, action="store_true",
-                help="set logging to verbose mode")
-    group.add_argument("-q", "--quiet", dest="quiet",
-                default=False, action="store_true",
-                help="set logging to quiet mode")
-
-    args = parser.parse_args()
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    elif not args.quiet:
-        logging.basicConfig(level=logging.INFO)
-
+    args = parse_args(doc=__doc__, prehook=setup_arguments)
     s = datetime.date(args.year, args.month, args.day)
     logging.debug("Start: %s", s)
-    for i in xrange(args.days):
+    for i in range(args.days):
         if args.reverse:
             t = s - datetime.timedelta(days=i)
         else:
             t = s + datetime.timedelta(days=i)
         args.output.write(t.strftime(args.format))
-        args.output.write('\n')
+        args.output.write("\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-# vim: set et ts=4 sw=4 cindent fileencoding=utf-8 :
