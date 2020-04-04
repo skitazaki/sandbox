5d3b771919794f599d722acebce6240e3b07ee80
Shigeru Kitazaki <skitazaki@gmail.com>
2020-04-04 11:34:44 +0900

    Simple Flask app to server contents in JavaScript playground.

--- src/javascript/playground/loop-test.js
+++ src/javascript/playground/loop-test.js
@@ -37,3 +37,33 @@ var Loopy = {
         return end - start;
     }
 };
+
+var TestPatterns = {
+    "naive": naiveLoop,
+    "dd":    duffsDevice
+};
+
+document.forms[0].onsubmit = function(e) {
+    e.preventDefault();
+    var elements = this.elements,
+        count = parseInt(elements["loopcount"].value),
+        loopTypeInput = elements["looptype"],
+        testType, result;
+    if (isNaN(count)) {
+        alert("Enter an interger value.");
+        return false;
+    }
+    Loopy.count = count;
+    for (var i = 0, len = loopTypeInput.length; i < len; i++) {
+        if (loopTypeInput[i].checked)
+            testType = loopTypeInput[i].value;
+    }
+    if (testType in TestPatterns) {
+        result = Loopy.run(TestPatterns[testType]);
+        document.getElementById("results").innerHTML += count + " " +
+            testType + " " + result + "<br/>";
+    } else {
+        alert("Invalid loop type.");
+    }
+    return false;
+};
---
+++ src/python/sandboxlib/httpsrv.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""Helper HTTP server for JavaScript playground.
+
+$ export FLASK_APP=sandboxlib.httpsrv
+$ export FLASK_ENV=development
+$ export SANDBOX_STATIC_DIR=$PWD/../javascript/playground
+$ export SANDBOX_TEMPLATE_DIR=$PWD/../javascript/playground
+$ flask run
+"""
+
+import os
+from pathlib import Path
+
+from flask import Flask, redirect, render_template
+
+
+def create_app(test_config=None):
+    try:
+        from dotenv import load_dotenv, find_dotenv
+
+        load_dotenv(find_dotenv())  # Load an upper level file as well
+    except ImportError:
+        pass
+
+    app = Flask(__name__, instance_relative_config=True, static_url_path="/static")
+    if static_dir := os.getenv("SANDBOX_STATIC_DIR"):
+        app.static_folder = static_dir
+    if template_dir := os.getenv("SANDBOX_TEMPLATE_DIR"):
+        app.template_folder = template_dir
+
+    @app.route("/<name>.html")
+    def hello(name):
+        static_dir = Path(app.static_folder)
+        use_css = True if (static_dir / f'{name}.css').exists() else False            
+        use_js = True if (static_dir / f'{name}.js').exists() else False            
+        return render_template(f"{name}.html", name=name, use_css=use_css, use_js=use_js)
+
+    return app
