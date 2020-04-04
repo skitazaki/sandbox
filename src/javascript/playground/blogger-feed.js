c8e2c93707bf8efccd695cf7677fa361f66cbef3
Shigeru Kitazaki <skitazaki@gmail.com>
2020-04-04 21:52:26 +0900

    Use React 16 without JSX

--- src/javascript/playground/blogger-feed.js
+++ src/javascript/playground/blogger-feed.js
@@ -1,7 +1,7 @@
 var FeedReader = {
-    "posts" : 5,
-    "chars" : 100,
-    read : function(blog_id) {
+    "posts": 5,
+    "chars": 100,
+    read: function(blog_id) {
         var script = document.createElement("script");
         var feed_url = "http://" + blog_id + ".blogspot.com";
         feed_url += "/feeds/posts/default";
@@ -12,7 +12,7 @@ var FeedReader = {
         /* this method should be rewritten to check script is executed after safe loading */
         setTimeout(function() { document.body.removeChild(script); }, 5000);
     },
-    parse : function(json) {
+    parse: function(json) {
         if (json == null || json.feed == "undefined") {
             try { console.log("fail to load json data."); } catch (ignore) {}
             return;
@@ -55,8 +55,108 @@ var FeedReader = {
         }
         this.callback(entries);
     },
-    callback : function(entries) {
+
+    callback: function(entries) {
         /* pass, override me :D */
     }
+};
+
+const e = React.createElement;
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '', error: false };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    if (this.state.value) {
+      this.setState({['error']: false});
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (!this.state.value) {
+      this.setState({['error']: true});
+      return;
+    }
+    if (this.props.onSubmit) {
+      this.props.onSubmit(this.state.value);
+    }
+  }
+
+  render() {
+    var cls = '';
+    if (this.state.error) {
+      cls = ' alert alert-danger';
+    }
+    return (
+      e('form',
+        { onSubmit: this.handleSubmit },
+        e('div', {
+          className: "input-group" },
+          e('div', {
+            className: "input-group-prepend" },
+            e('span', {
+              className: "input-group-text" }, 'http://')),
+          e('input', {
+            className: "form-control" + cls,
+            placeholder: "Blogger ID",
+            maxlength: 20,
+            onChange: this.handleChange }),
+          e('div', {
+            className: "input-group-append" },
+            e('span', {
+              className: "input-group-text" }, '.blogspot.com')),
+          e('button', { className: "btn btn-primary", onClick: this.handleSubmit }, 'Read')))
+    );
+  }
 }
 
+class BloggerFeedReader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { blogId: "", entries: null };
+    FeedReader.posts = 10;
+    FeedReader.chars = 140;
+  }
+
+  read(self, blogId) {
+    self.setState({["blogId"]: blogId});
+    self.setState({["entries"]: []});
+    FeedReader.callback = function(entries) {
+      self.setState({["entries"]: entries});
+    };
+    try {
+      FeedReader.read(blogId);
+    } catch (e) {
+      try { console.log(e); } catch (ignore) {}
+    }
+  }
+
+  renderEntries() {
+    const entries = this.state.entries;
+    var articles;
+    if (entries) {
+      articles = entries.map(element => {
+        return e("div", { className: "row" },
+                 e("h3", {}, element["title"]),
+                 e("p", {},
+                   element["content"] + " ...",
+                   e("a", { href: element["url"] }, "More"),
+                   e("span", {}, "(" + element["date"] + ")")));
+      });
+    }
+    return e("div", {}, articles);
+  }
+
+  render() {
+    return e('div', {},
+             e(NameForm, { onSubmit: (blogId) => this.read(this, blogId) }),
+             this.renderEntries());
+  }
+}
